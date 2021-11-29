cf6783d86ba0430981fbb98545e6d8987a110cf5
Noor Ahmed <mildk123@gmail.com>
2021-11-29 17:53:18 +0500

    Added Admin Panel UI

---
+++ AlVigha/Controllers/Admin/AdminCustomersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AlVigha.Models;
+
+namespace AlVigha.Controllers.Admin
+{
+    public class AdminCustomersController : Controller
+    {
+        AlVighaEntities DB = new AlVighaEntities();
+        // GET: AdminCustomers
+        public ActionResult Index()
+        {
+            List<Account> customersList = DB.Accounts.Where(x => x.account_type == 2).ToList();
+            return View("~/Views/Admin/AdminCustomers.cshtml", customersList);
+            //}
+        }
+    }
+}
